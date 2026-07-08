984a4d56ddf82ac75cf10cd07c09aa114fb6f8d1
RAVIKISHAN GAUR <ravikishan202t@gmail.com>
2026-07-08 08:59:05 +0530

    Time: 138 ms (6.67%) | Memory: 251.5 MB (6.67%) - LeetSync

---
+++ 4136-concatenate-non-zero-digits-and-multiply-by-sum-ii/concatenate-non-zero-digits-and-multiply-by-sum-ii.java
@@ -0,0 +1,82 @@
+class Solution {
+    static final int MOD = 1000000007;
+
+    class Node {
+        long num;
+        long sum;
+        int cnt;
+
+        Node() {}
+
+        Node(long num, long sum, int cnt) {
+            this.num = num;
+            this.sum = sum;
+            this.cnt = cnt;
+        }
+    }
+
+    Node[] tree;
+    long[] pow10;
+
+    public int[] sumAndMultiply(String s, int[][] queries) {
+        int n = s.length();
+
+        pow10 = new long[n + 1];
+        pow10[0] = 1;
+        for (int i = 1; i <= n; i++) {
+            pow10[i] = (pow10[i - 1] * 10) % MOD;
+        }
+
+        tree = new Node[4 * n];
+        build(1, 0, n - 1, s);
+
+        int[] ans = new int[queries.length];
+
+        for (int i = 0; i < queries.length; i++) {
+            Node res = query(1, 0, n - 1, queries[i][0], queries[i][1]);
+            ans[i] = (int) ((res.num * res.sum) % MOD);
+        }
+
+        return ans;
+    }
+
+    void build(int idx, int l, int r, String s) {
+        if (l == r) {
+            int d = s.charAt(l) - '0';
+            if (d == 0)
+                tree[idx] = new Node(0, 0, 0);
+            else
+                tree[idx] = new Node(d, d, 1);
+            return;
+        }
+
+        int mid = (l + r) / 2;
+        build(idx * 2, l, mid, s);
+        build(idx * 2 + 1, mid + 1, r, s);
+
+        tree[idx] = merge(tree[idx * 2], tree[idx * 2 + 1]);
+    }
+
+    Node merge(Node a, Node b) {
+        Node res = new Node();
+        res.cnt = a.cnt + b.cnt;
+        res.sum = a.sum + b.sum;
+        res.num = (a.num * pow10[b.cnt] + b.num) % MOD;
+        return res;
+    }
+
+    Node query(int idx, int l, int r, int ql, int qr) {
+        if (ql > r || qr < l)
+            return new Node(0, 0, 0);
+
+        if (ql <= l && r <= qr)
+            return tree[idx];
+
+        int mid = (l + r) / 2;
+
+        Node left = query(idx * 2, l, mid, ql, qr);
+        Node right = query(idx * 2 + 1, mid + 1, r, ql, qr);
+
+        return merge(left, right);
+    }
+}
