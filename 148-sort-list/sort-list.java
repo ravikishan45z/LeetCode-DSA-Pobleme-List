dec9f02e62caef58bf021003869e7dc0a1d7c9ad
RAVIKISHAN GAUR <ravikishan202t@gmail.com>
2026-07-09 10:00:47 +0530

    Time: 9 ms (99.18%) | Memory: 59.4 MB (46.53%) - LeetSync

---
+++ 148-sort-list/sort-list.java
@@ -0,0 +1,60 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode merge(ListNode head1, ListNode head2) {
+        ListNode dummy = new ListNode(-1);
+        ListNode tail = dummy;
+
+        while (head1 != null && head2 != null) {
+            if (head1.val <= head2.val) {
+                tail.next = head1;
+                head1 = head1.next;
+            } else {
+                tail.next = head2;
+                head2 = head2.next;
+            }
+            tail = tail.next;
+        }
+
+        if (head1 != null) {
+            tail.next = head1;
+        } else {
+            tail.next = head2;
+        }
+
+        return dummy.next;
+    }
+
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        // Step1 :spilit into two equal parts.
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // step 2 = sort recursively;
+        ListNode head2 = slow.next;
+        slow.next = null;
+
+        ListNode head1 = head;
+
+        head1 = sortList(head1);
+        head2 = sortList(head2);
+
+        // merge it and Return it
+        return merge(head1, head2);
+    }
+}
