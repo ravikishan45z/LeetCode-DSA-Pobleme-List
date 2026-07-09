3a0469dba23615cf796dfb3618616942ad3ea0b8
RAVIKISHAN GAUR <ravikishan202t@gmail.com>
2026-07-09 09:40:22 +0530

    Time: 1 ms (98.66%) | Memory: 47.5 MB (22.42%) - LeetSync

---
+++ 724-find-pivot-index/find-pivot-index.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int n = nums.length;
+
+        // Left prefix sum Array.
+        int[] leftSum = new int[n];
+        leftSum[0]= nums[0];
+        for(int i = 1; i<n; i++){
+            leftSum[i] = leftSum[i-1] + nums[i];
+        }
+
+        // Right prefix sum Array.
+        int[] rightSum = new int[n];
+        rightSum[n-1] = nums[n-1];
+        for(int i = n-2; i>=0; i--){
+            rightSum[i] = rightSum[i+1] + nums[i];
+        }
+    
+        // Comparasion.
+        for(int i = 0; i<n; i++){
+            if(leftSum[i] == rightSum[i]){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
