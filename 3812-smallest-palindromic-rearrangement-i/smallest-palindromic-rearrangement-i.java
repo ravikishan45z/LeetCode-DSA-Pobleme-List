2a9c64988d29713bbd93585e34ae953441eafd75
RAVIKISHAN GAUR <ravikishan202t@gmail.com>
2026-07-28 09:09:27 +0530

    Time: 32 ms (53.82%) | Memory: 48.3 MB (38.15%) - LeetSync

---
+++ 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String smallestPalindrome(String s) {
+        int n = s.length();
+
+        char[] half = s.substring(0, n / 2).toCharArray();
+        Arrays.sort(half);
+
+        String rev = new String(half) + ((n & 1) == 1 ? s.charAt(n / 2) : "")
+                + new StringBuilder(new String(half)).reverse();
+
+        return rev;
+    }
+}
