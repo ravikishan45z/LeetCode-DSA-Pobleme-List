d964ad5d8c87ebd118190adc9ecde61eeb8e13d0
RAVIKISHAN GAUR <ravikishan202t@gmail.com>
2026-07-29 09:28:30 +0530

    Time: 17 ms (77.53%) | Memory: 45.8 MB (14.18%) - LeetSync

---
+++ 16-3sum-closest/3sum-closest.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int n = nums.length;
+
+        int close = nums[0] + nums[1] + nums[2];
+
+        for(int i = 0; i<n -2; i++){
+            int l = i+1;
+            int r = n-1;
+
+            while(l < r){
+                int sum = nums[i] + nums[l] + nums[r];
+                
+                if(Math.abs(sum - target) < Math.abs(close - target)){
+                    close = sum;
+                }
+
+                if(sum < target){
+                    l++;
+                }else if(sum > target){
+                    r--;
+                }else{
+                    return sum; //mil gaya exact target.
+                }
+            }
+        }
+
+        return close;
+    }
+}
