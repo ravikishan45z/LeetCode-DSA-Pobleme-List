ff51afe98edd38e47b0d5ff5bee6316dc9f91fae
RAVIKISHAN GAUR <ravikishan202t@gmail.com>
2026-07-30 10:35:37 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (30.19%) - LeetSync

---
+++ 404-sum-of-left-leaves/sum-of-left-leaves.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int sumOfLeftLeaves(TreeNode root) {
+        int sum = 0;
+        if(root == null) return sum;
+
+        if(root.left != null && root.left.left == null && root.left.right == null){
+            sum += root.left.val;
+        } else {
+            sum += sumOfLeftLeaves(root.left);
+        }
+
+        sum += sumOfLeftLeaves(root.right);
+
+        return sum;
+    }
+}
